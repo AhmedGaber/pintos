5b606c04d2402dc57b0e138fe523747cc7d58b58
Ahmed Gaber <ahmedgaber20@gmail.com>
2015-12-20 21:14:08 +0200

    Add basic implementation of system call infrastructure

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,9 +6,16 @@
 #include "threads/vaddr.h"
 #include "userprog/pagedir.h"
 #include <stdbool.h>
+#include "devices/shutdown.h"
 
 static void syscall_handler (struct intr_frame *);
 static int memread (void *src, void *des, size_t bytes);
+static int32_t get_user (const uint8_t *uaddr);
+static bool put_user (uint8_t *udst, uint8_t byte);
+
+void sys_halt (void);
+void sys_exit (int status);
+
 
 void
 syscall_init (void)
@@ -28,7 +35,50 @@ syscall_handler (struct intr_frame *f)
     return;
   }
 
-  printf ("DEBUG >>> System call number = %d\n", syscall_number);
+  printf ("DEBUG >>> System call number = %d.\n", syscall_number);
+
+  /* Dispatch w.r.t system call number.
+     All Constants are defined in /lib/syscall-nr.h */
+  switch (syscall_number)
+  {
+
+  case SYS_HALT:
+  {
+    sys_halt();
+    NOT_REACHED();
+    break;
+  }
+
+  case SYS_EXIT:
+  {
+    int exitcode;
+    if (memread(f->esp + 4, &exitcode, sizeof(exitcode)) == -1)
+      thread_exit(); // invalid memory access attampet.
+
+    sys_exit(exitcode);
+    NOT_REACHED();
+    break;
+  }
+
+  case SYS_EXEC:
+  case SYS_WAIT:
+  case SYS_CREATE:
+  case SYS_REMOVE:
+  case SYS_OPEN:
+  case SYS_FILESIZE:
+  case SYS_READ:
+  case SYS_WRITE:
+  case SYS_SEEK:
+  case SYS_TELL:
+  case SYS_CLOSE:
+
+  /* unhandled case */
+  default:
+    printf("ERROR >>> System call %d is unimplemented.\n", syscall_number);
+    thread_exit();
+    break;
+  }
+
   thread_exit ();
 }
 
@@ -85,4 +135,27 @@ memread (void *src, void *dst, size_t bytes)
     *(char*)(dst + i) = value & 0xff; // from the manual.
   }
   return (int)bytes;
+}
+
+/**
+* Terminates Pintos by calling shutdown_power_off() (declared in
+* ‘devices/shutdown.h’).
+*/
+void
+sys_halt(void)
+{
+  shutdown_power_off(); // from devices/shutdown.h
+}
+
+/**
+* Terminates the current user program, returning status to the kernel. If the
+* process’s parent waits for it, this is the status that will be returned.
+*/
+void
+sys_exit(int status UNUSED)
+{
+  printf("%s: exit(%d)\n", thread_current()->name, status);
+
+  // TODO
+  thread_exit();
 }
