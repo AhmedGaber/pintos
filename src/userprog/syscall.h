6e9961dd31bf124f0d5d042b46bde6d9cf0f2c46
Mostafa Gabriel <gabriel.mostafa@gmail.com>
2015-12-23 19:49:41 +0200

    Update syscall.h

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,25 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include <list.h>
+
 void syscall_init (void);
+
+struct child_process{
+  pid_t pid;
+  struct list_elem elem;
+  struct semaphore wait_load;
+  struct semaphore waiting;
+  int loaded;
+  bool exited;
+  bool wait;
+  int exit_state;
+}
+
+struct file_desc {
+  int id;
+  struct list_elem elem;
+  struct file* file;
+};
 
 #endif /* userprog/syscall.h */
